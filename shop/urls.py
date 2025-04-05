f025a01d29c50ad43e593c73e0c7f71cc9004154
BarMerAla <meeke.mr@bk.ru>
2025-04-05 20:12:08 +0500

    add re_path

--- shop/urls.py
+++ shop/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,9 +29,13 @@ urlpatterns = [
     
 ]
 
-
+# Отдача static и media в режиме DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  
+ 
     
-
+# Всегда отдавать media вручную — если не DEBUG и Render не справляется
+urlpatterns = [
+re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
